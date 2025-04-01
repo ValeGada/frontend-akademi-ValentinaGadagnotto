ca9c2cd4c7112d106f7496537672d6780832b27e
ValeGada <valegadagnotto@gmail.com>
2025-04-01 16:19:26 -0300

    Update ProductsList
    Turn list to table

--- proyecto-entregable/src/components/ProductsList.js
+++ proyecto-entregable/src/components/ProductsList.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchProducts } from "../actions";
@@ -9,24 +10,32 @@ const ProductsList = ({fetchProducts, products}) => {
     }, [])
     
     return (
-        <div>
-            <h2>Productos</h2>
-            <div className="ui celled list">
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Categoría</th>
+                    <th>Precio</th>
+                </tr>
+            </thead>
+            <tbody>
                 {products && products.length > 0 ? (
-                    products.map(product => (
-                    <div className="item" key={product.id}>
-                        <div className="content">
-                            {product.name}
-                            <div className="description">{product.description}</div>
-                        </div>
-                    </div>
-                    ))
-                ) : (
-                    <p>No hay productos disponibles</p>
+                    products.map( product => (
+                        <tr key={product.id}>
+                            <td>{product.name}</td>
+                            <td>{product.category}</td>
+                            <td>${product.price}</td>
+                        </tr>
+                        ))
+                    ) : (
+                    <tr>
+                        <td>No hay información disponible</td>
+                    </tr>
                 )}
-            </div>
-        </div>
+            </tbody>
+        </table>
     );
+    
 }
 
 const mapStateToProps = state => {
